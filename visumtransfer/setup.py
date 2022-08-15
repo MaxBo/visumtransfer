524bf0c6be9b1e2b725a5066dd9dfbe70bed2c38
MaxBo <bohnet@ggr-planung.de>
2022-08-15 17:54:20 +0200

    visumtransfer_attributes_with_range

--- visumtransfer/setup.py
+++ visumtransfer/setup.py
@@ -6,14 +6,14 @@ Created on Fri Jun 10 20:33:08 2016
 """
 import numpy as np
 
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_packages
 
 
 setup(
     name="visumtransfer",
     version="0.1",
     description="Write Visum-Transfer Files",
-    packages=find_namespace_packages(include=['visumtransfer.*']),
+    packages=find_packages('src'),
 
     package_dir={'': 'src'},
     include_package_data=True,
