76702364040fe77fad000c6ec4182bc58d9fecf9
MaxBo <bohnet@ggr-planung.de>
2023-04-24 13:44:03 +0200

    setup.py/version for visumtransfer

--- visumtransfer/setup.py
+++ visumtransfer/setup.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Jun 10 20:33:08 2016
 
-@author: MaxBohnet
-"""
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 
 setup(
     name="visumtransfer",
     description="Write Visum-Transfer Files",
-    packages=find_packages('src'),
-    namespace_packages=['visumtransfer'],
+    packages=find_namespace_packages('src'),
+    #namespace_packages=['visumtransfer'],
     package_dir={'': 'src'},
     package_data={'': ['attributes.h5'], },
     include_package_data=True,
